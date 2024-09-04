e2b7e4e07a824fd93f3c552c0d20f9f3eba0fe2a
Ankit Singh <131437171+anku909@users.noreply.github.com>
2024-09-05 00:34:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Its Deploy automatically.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
       </header>
     </div>
   );
